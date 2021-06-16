b0658f5573282123fd09abe1f8de3d6750a85b46
David Rolfe <drolfe@voltdb.com>
2021-06-16 09:32:56 +0100

    remove jar xvf as not needed any more

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,5 +9,4 @@ sqlcmd --servers=vdb1 < ../ddl/create_db.sql
 sqlcmd --servers=vdb1 < ../ddl/demo_db.sql
 java -jar $HOME/bin/addtodeploymentdotxml.jar vdb1 deployment topics.xml
 
-cd ../jars
-jar xvf voltdb-javacache-demo.jar
+
