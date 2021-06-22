ff0a25cdb2eed3cf404412ede3399d5f30adc133
David Rolfe <drolfe@voltdb.com>
2021-06-22 18:50:14 +0100

    fixed user and class loading issue

--- jars/runworkload.sh
+++ jars/runworkload.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+#
+# Load classes and create users
+#
+
+java -jar voltdb-javacache-demo-client.jar `cat $HOME/.vdbhostnames` 10000000 1 3 100 1000 1
+
 for i in 2 4 8 12 16 20 24 28 32 
 do 
-	java -jar voltdb-javacache-demo-client.jar vdb1,vdb2,vdb3 10000000 $i 300 100 1000 0  > $i.lst
+	java -jar voltdb-javacache-demo-client.jar  `cat $HOME/.vdbhostnames` 10000000 $i 300 100 1000 0  > $i.lst
 done
