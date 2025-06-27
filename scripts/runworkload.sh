e36abf24f766487e22c02817de3ae5a97a5004c5
dwrolfe <david.rolfe@pobix.com>
2025-06-27 12:51:27 +0100

    run_workload

--- scripts/runworkload.sh
+++ scripts/runworkload.sh
@@ -4,11 +4,18 @@
 # Load classes and create users
 #
 
+
+. $HOME/.profile
+
 USERCOUNT=1000000
 
-java -jar voltdb-javacache-demo-client.jar `cat $HOME/.vdbhostnames` ${USERCOUNT} 10 3 100 1000 1 voltdb-javacache-demo-client.jar
+cd ../jars
 
-for i in 2 4 8 12 16 20 24 28 32 
-do 
-	java -jar voltdb-javacache-demo-client.jar  `cat $HOME/.vdbhostnames` ${USERCOUNT} $i 300 100 1000 0 voltdb-javacache-demo-client.jar > $i.lst
+java ${JVMOPTS} -cp /voltdb-javacache-sandbox/jars -jar voltdb-javacache-demo-client.jar `cat $HOME/.vdbhostnames` ${USERCOUNT} 1 3 100 1000 1 0
+
+exit 0
+
+for i in 2 4 8 12 16 20 24 28 32
+do
+        java ${JVMOPTS} -jar voltdb-javacache-demo-client.jar  `cat $HOME/.vdbhostnames` ${USERCOUNT} $i 300 100 1000  1 0 > $i.lst
 done
